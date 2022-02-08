e940f944828bb8e872b203bb00d6a9c345383814
PatrickLBarbosa <patrick.lins1@live.com>
2022-02-08 07:20:36 -0300

    Exercício notion variáveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,79 @@
+/*
+ Patrick Lins Barbosa
+1- (10,5)
+
+2- (10,10,10)
+
+3- 
+let horasPorDia = prompt("Quantas horas você trabalha por dia?")
+let salarioPorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${salarioPorDia/horasPorDia} por hora`)
+
+*/
+
+
+//                          EXERCICIO DE ESCRITA DE CÓDIGO
+
+
+const nome =undefined  
+console.log (typeof nome)
+//undefined
+
+
+let idade = undefined
+console.log (typeof idade)
+//undefined
+
+// typeof de nome e idade, é undefined, pois não há valor atribuido.
+
+
+
+const nome = prompt('Qual é o seu nome? ')
+console.log (nome)
+const idade = prompt('Qual a sua idade? ')
+console.log (idade)
+
+const nome = ('Patrick')
+console.log (nome)
+const idade = (27)
+console.log (idade)
+
+/* 
+    A primeira variável recebe o valor Patrick, e a classificação dessa variável é string , pois são formados por conjutos de caracteres em " ".
+    No segundo caso, temos um number, pois são não estão em string " " e seu valor é um número.
+*/
+
+
+const nome = ('Patrick')
+console.log (nome)
+const idade = (27)
+console.log (idade)
+
+console.log ('Olá ' + (nome) + ' você tem ' + (idade) + ' anos')
+ Olá Patrick você tem 27 anos
+
+
+
+2: 
+let acordar = prompt ('Você acordou cedo hoje? ')
+console.log (acordar)
+Sim
+
+let jornal = prompt ('Você leu algum jornal?' )
+console.log(jornal)
+Não
+
+let chuva = prompt ('Tem chovido por ai? ') 
+console.log(chuva) 
+ Sim 
+ 
+ 3:
+ let a = 10
+ let b = 25
+
+ a = b 
+ let b = 10 
+
+
+
+
