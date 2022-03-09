ac547990aa364cf335969bdfd6bae881a918fec7
PatrickLBarbosa <patrick.lins1@live.com>
2022-03-08 23:23:19 -0300

    Exercicio laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,82 @@
+// Exercícios de Interpretação:
+/* 
+1- o que o código está fazendo?
+ let valor = 0;
+ for(let i = 0; 1 < 5; 1++){
+     valor += 1
+ }
+
+R: Neste caso, o código irá realizar uma contagem até o número 5.
+
+
+ 2- Leia o código abaixo:
+ */
+/*  const lista = [10,11,12,15,18,19,21,23,25,27,30]
+for(let numero of lista){
+    if (numero > 18) {
+        console.log(numero)
+    }
+}   */
+/* 
+ R: 
+ a) Será impresso somente os números acima de 18.
+ 
+ b) 
+
+
+ 3- Qual seria o resultado impresso no console caso o usuário digitasse o número 4?
+    
+    será impresso um loop de um ao 4 asteristicos.
+
+
+    Exercício de Escrita de Código:
+
+
+    1-
+*/
+ let bichinhosDeEstimacao = Number(prompt("Quantos bichinhos de estimação você tem? "))
+
+/* let quantidadeDePets = 0; */
+
+if (bichinhosDeEstimacao === 0) {
+    console.log('Que pena! Você pode adotar um pet!')
+
+
+
+
+/* else if (bichinhosDeEstimacao > 0) {
+    console.log = prompt["digite o nome dos pets"]
+    
+} */
+ 
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+/* 
+}while (bichinhosDeEstimacao > 0) {
+    
+} */
+
+
+
+
+
+/* else if (bichinhosDeEstimacao > 0) {
+    let arrayNomePets = prompt('Digite o nome dos Pets')
+}
+console.log(arrayNomePets) */
+
+
+}
+
+
