eefa82b8a805e55e48bea7487517c575a40ea946
PatrickLBarbosa <patrick.lins1@live.com>
2022-03-18 21:28:17 -0300

    Entrega resolvendo problemas

--- modulo1/resolvendo-problemas/lista-revisao-js/exercicios.js
+++ modulo1/resolvendo-problemas/lista-revisao-js/exercicios.js
@@ -5,49 +5,105 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-    return array.length
-   }
+  return array.length
+}
 
 // EXERCÍCIO 02
-function retornaArrayInvertido (array) {
+function retornaArrayInvertido(array) {
   return array.reverse()
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    array.sort(function(a,b){
-        return a > b 
-    })
+  /* array.sort(function(a,b){
+   if(a > b) return 1;
+   if(a < b) return -1;
+   return 0;  Não funcionou!
+ }) */
+  return array.sort((a, b) => a - b)
 }
 
 
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  array = [2,45,67,5,15]
-  if (array % 2 === 0){
-      return novoArray
-  } 
+  const retornaPares = array.filter((item) => {
+    return item % 2 === 0
+  })
+  return retornaPares
 }
 
-// EXERCÍCIO 05
+
+
+// EXERCÍCIO 05 
 function retornaNumerosParesElevadosADois(array) {
- 
+  //NÃO FUNCIONA
+  const paresElevado = array.filter((index) => {
+    if (index % 2 === 0) {
+    }
+    return paresElevado ** 2
+  })
 }
+
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  return Math.max(...array)
 }
+
+
+
+
+
 
 // EXERCÍCIO 07
-function retornaObjetoEntreDoisNumeros(num1, num2) {
+function retornaObjetoEntreDoisNumeros
+(num1, num2) {
+  if (num1 > num2) {
+    return num1
+  } else {
+    return num2
+  } 
+} 
 
+// bloco 
+const declaraBloco = {
+  
 }
+console.log(retornaObjetoEntreDoisNumeros(5,6))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* if (num1 > num2) {
+    return num1
+  } else {
+    return num2
+  } 
+} 
+console.log(retornaObjetoEntreDoisNumeros(num1, num2)) */
+
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+
 }
 
 // EXERCÍCIO 09
@@ -57,27 +113,27 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
@@ -87,10 +143,13 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+
 }
+
+
+
