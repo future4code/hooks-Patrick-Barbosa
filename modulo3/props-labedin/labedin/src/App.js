773edef7a51dcdea75f3d209575a15403c9c0ba2
Patrick L Barbosa <patrick.lins1@live.com>
2022-05-04 14:45:51 -0300

    Exercício Labedin Styled Components & Props

--- modulo3/props-labedin/labedin/src/App.js
+++ modulo3/props-labedin/labedin/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
+import CardPequeno from './components/CardPequeno/CardPequeno';
 import ImagemButton from './components/ImagemButton/ImagemButton';
-import Foto from "../src/components/Imagem/Foto.jpg"
+import CardLocation from './components/CardLocation/CardLocation';
 
 
 function App() {
@@ -10,44 +11,56 @@ function App() {
     <div className="App">
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
-        <CardGrande 
-          imagem="{Foto}" 
-          nome="Patrick Barbosa" 
-          descricao="Oi, eu sou o Patrick, tenho 27 anos e estou aprendendo React js, minha dificuldade esta sendo em trocar a imagem."
+        <CardGrande
+          imagem="/FotoEu.jpg"
+          nome="Patrick Barbosa"
+          descricao="Olá, me chamo Patrick, gosto muito de programação apesar de ser dificil, devemos persistir sempre nos nossos objetivos."
         />
-        
-        <ImagemButton 
-          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+
+        <ImagemButton
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png"
           texto="Ver mais"
         />
       </div>
 
+      <div>
+        <CardPequeno
+        email="labenu@gmail.com"
+        />
+      </div>
+
+     { <div>
+        <CardLocation
+        local="Estrada Labenu, 404"
+        />
+      </div>}
+
       <div className="page-section-container">
         <h2>Experiências profissionais</h2>
-        <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        <CardGrande
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png"
+          nome="Labenu"
+          descricao="Formando desenvolvedores para o mercado de trabalho!"
         />
-        
-        <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+
+        <CardGrande
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg"
+          nome="NASA"
+          descricao="Apontando defeitos."
         />
       </div>
 
       <div className="page-section-container">
         <h2>Minhas redes sociais</h2>
-        <ImagemButton 
-          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
-          texto="Facebook" 
-        />        
+        <ImagemButton
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png"
+          texto="Facebook"
+        />
 
-        <ImagemButton 
-          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
-          texto="Twitter" 
-        />        
+        <ImagemButton
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png"
+          texto="Twitter"
+        />
       </div>
     </div>
   );
